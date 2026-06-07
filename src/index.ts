53ed4ef829cc09eb00eed07b8f4385cc64e30ef4
Thélio <theliomt@gmail.com>
2026-06-07 00:34:55 -0300

    fix: expose tui from package root

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { serverPlugin as default } from "./server/index";
+import { serverPlugin } from "./server/index";
+import tuiModule from "./tui/tui";
+
+export default Object.assign(serverPlugin, tuiModule);
